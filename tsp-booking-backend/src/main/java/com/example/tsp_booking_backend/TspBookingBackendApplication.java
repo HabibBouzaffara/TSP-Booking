dacb796625fa126db2f7b6fbf734aec73ccda0c1
Habib Bouzoffara <habib.bouzaffara@ieee.org>
2026-01-04 18:02:19 +0100

    Init

---
+++ tsp-booking-backend/src/main/java/com/example/tsp_booking_backend/TspBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsp_booking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TspBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TspBookingBackendApplication.class, args);
+	}
+
+}
