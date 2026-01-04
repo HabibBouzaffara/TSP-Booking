c451ee6989d9be94ea213f541f33777f3b91908c
Habib Bouzoffara <habib.bouzaffara@ieee.org>
2026-01-04 18:18:37 +0100

    Initial

---
+++ tsp-booking-backend/src/test/java/com/example/tsp_booking_backend/TspBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsp_booking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TspBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
